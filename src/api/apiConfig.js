22474fcc321427d67cc7d1523ddf40a456b780e4
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-07-17 13:53:31 -0500

    Firts Commit

---
+++ src/api/apiConfig.js
@@ -0,0 +1,6 @@
+export const apiConfig = {
+  baseUrl: 'https://api.themoviedb.org/3/',
+  apiKey: 'f1da2e96854754de41624ac489a8286f',
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
