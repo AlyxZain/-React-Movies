c7d772f5cc0f2853e204853b694fffb794b2f9ab
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-07-17 18:55:48 -0500

    Api Terminada V2

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -19,31 +19,38 @@ export const tvType = {
 
 export const tmdbApi = {
   getMoviesList: (type, params) => {
-    const url = 'movie/' + movieType[type];
+    //const url = category[cate] + '/' + id + '/videos';
+    const url = `/movie/${type}`;
     return axiosClient.get(url, params);
   },
   getTvList: (type, params) => {
-    const url = 'tv/' + tvType[type];
+    // const url = 'tv/' + tvType[type];
+    const url = `/tv/${type}`;
     return axiosClient.get(url, params);
   },
   getVideos: (cate, id) => {
-    const url = category[cate] + '/' + id + '/videos';
+    // const url = category[cate] + '/' + id + '/videos';
+    const url = `/${cate}/${id}/videos`;
     return axiosClient.get(url, { params: {} });
   },
   search: (cate, params) => {
-    const url = 'search/' + category[cate];
+    // const url = 'search/' + category[cate];
+    const url = `/search/${cate}`;
     return axiosClient.get(url, params);
   },
   detail: (cate, id, params) => {
-    const url = category[cate] + '/' + id;
+    // const url = category[cate] + '/' + id;
+    const url = `/${cate}/${id}`;
     return axiosClient.get(url, params);
   },
   credits: (cate, id) => {
-    const url = category[cate] + '/' + id + '/credits';
+    // const url = category[cate] + '/' + id + '/credits';
+    const url = `/${cate}/${id}/credits`;
     return axiosClient.get(url, { params: {} });
   },
   similar: (cate, id) => {
-    const url = category[cate] + '/' + id + '/similar';
+    // const url = category[cate] + '/' + id + '/similar';
+    const url = `/${cate}/${id}/similar`;
     return axiosClient.get(url, { params: {} });
   },
 };
