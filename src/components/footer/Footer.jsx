f7bed7fd9d4a578e893a8490d739c701bab3d5dc
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-07-17 18:57:51 -0500

    Footer Terminado V1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,20 +20,12 @@ export const Footer = () => {
         <div className='footer__content__menus'>
           <div className='footer__content__menu'>
             <Link to='/'>Home</Link>
-            <Link to='/'>Contact us</Link>
-            <Link to='/'>Term of services</Link>
-            <Link to='/'>About us</Link>
           </div>
           <div className='footer__content__menu'>
-            <Link to='/'>Live</Link>
+            <Link to='/contact'>Contact us</Link>
+          </div>
+          <div className='footer__content__menu'>
             <Link to='/'>FAQ</Link>
-            <Link to='/'>Premium</Link>
-            <Link to='/'>Pravacy policy</Link>
-          </div>
-          <div className='footer__content__menu'>
-            <Link to='/'>You must watch</Link>
-            <Link to='/'>Recent release</Link>
-            <Link to='/'>Top IMDB</Link>
           </div>
         </div>
       </div>
