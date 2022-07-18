82586b5a372d65644aea0d040e2e9475328a623c
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-07-17 19:12:20 -0500

    V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
 
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+
+import { Header } from './components/header/Header';
+import { Footer } from './components/footer/Footer';
+
+import { Rutas } from './config/Rutas';
+
+export function App() {
   return (
     <>
-      <div>App</div>
+      <Header />
+      <Rutas />
+      <Footer />
     </>
   );
-};
+}
--- src/config/Rutas.jsx
+++ src/config/Rutas.jsx
@@ -2,9 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
 import { Catalog } from '../pages/Catalog';
-import { Detail } from '../pages/Detail';
+import { Detail } from '../pages/detail/Detail';
 import { Login } from '../pages/Login';
 import { Register } from '../pages/Register';
+import { Contact } from '../pages/contact/Contact';
 
 export const Rutas = () => {
   return (
@@ -14,6 +15,7 @@ export const Rutas = () => {
       <Route path='/:category/:id' element={<Detail />} />
       <Route path='/:category' element={<Catalog />} />
 
+      <Route path='/contact' element={<Contact />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
     </Routes>
